31ecb3e9880bc99d5113adbbe801962a0ccd8846
Aliaksandr <djvalentinoff@gmail.com>
2023-11-17 12:30:47 +0300

    feat: add sending data from the form

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ window.addEventListener("DOMContentLoaded", () => {
   /* Tabs end */
 
   /* Timer start */
-  const deadline = "2023-10-19";
+  const deadline = "2023-12-31";
 
   function getTimeRemaining(endtime) {
     let days;
@@ -108,7 +108,6 @@ window.addEventListener("DOMContentLoaded", () => {
   /* Modal start */
   const modalTrigger = document.querySelectorAll("[data-modal]");
   const modal = document.querySelector(".modal");
-  const modalCloseBtn = document.querySelector("[data-close]");
   const modalTimerId = setTimeout(openModal, 5000);
 
   function openModal() {
@@ -128,10 +127,8 @@ window.addEventListener("DOMContentLoaded", () => {
     btn.addEventListener("click", openModal);
   });
 
-  modalCloseBtn.addEventListener("click", closeModal);
-
   modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
+    if (e.target === modal || e.target.getAttribute('data-close') == '') {
       closeModal();
     }
   });
@@ -226,4 +223,89 @@ window.addEventListener("DOMContentLoaded", () => {
     "menu__item"
   ).render();
   /* Используем классы для карточек end */
+
+  /* Forms start */
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "img/form/spinner.svg",
+    success: "Спасибо! Скоро мы с вами свяжемся!",
+    failure: "Что-то пошло не так...",
+  };
+
+  forms.forEach(item => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement("img");
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+        display: block;
+        margin: 0 auto;
+      `;
+      form.insertAdjacentElement("afterend", statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open("POST", "server.php");
+
+      // если используем new XMLHttpRequest(), то заголовки устанавливать не нужно, они устанавливаются автоматически
+      // request.setRequestHeader("Content-type", "multipart/form-data");
+
+      // Для формата json устанвливаем заголовки
+      request.setRequestHeader("Content-type", "application/json");
+
+      const formData = new FormData(form);
+
+      const object = {};
+      formData.forEach((value, key) => {
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+
+      request.send(json);
+
+      request.addEventListener("load", () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          showThanksModal(message.success);
+          form.reset();
+          statusMessage.remove();
+        } else {
+          showThanksModal(message.failure);
+        }
+      });
+    });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector(".modal__dialog");
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add("modal__dialog");
+    thanksModal.innerHTML = `
+      <div class="modal__content">
+        <div class="modal__close" data-close>×</div>
+        <div class="modal__title">
+          ${message}
+        </div>
+      </div>
+    `;
+
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add("show");
+      prevModalDialog.classList.remove("hide");
+      closeModal();
+    }, 4000);
+  }
+  /* Forms end */
 });
