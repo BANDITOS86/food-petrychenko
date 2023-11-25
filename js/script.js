1b7629c7d366756f45ccaedafe44a585886291e2
Aliaksandr <djvalentinoff@gmail.com>
2023-11-25 12:34:44 +0300

    feat: add babel in project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+require("es6-promise").polyfill();
+import "nodelist-foreach-polyfill";
+
 import tabs from "./modules/tabs";
 import modal from "./modules/modal";
 import slider from "./modules/slider";
@@ -5,13 +8,13 @@ import calc from "./modules/calc";
 import cards from "./modules/cards";
 import forms from "./modules/forms";
 import timer from "./modules/timer";
-import { openModal } from './modules/modal';
+import { openModal } from "./modules/modal";
 
 window.addEventListener("DOMContentLoaded", () => {
-   const modalTimerId = setTimeout(
-     () => openModal(".modal", modalTimerId),
-     5000
-   );
+  const modalTimerId = setTimeout(
+    () => openModal(".modal", modalTimerId),
+    5000
+  );
 
   tabs(
     ".tabheader__item",
@@ -28,7 +31,7 @@ window.addEventListener("DOMContentLoaded", () => {
     totalCounter: "#total",
     currentCounter: "#current",
     wrapper: ".offer__slider-wrapper",
-    field: ".offer__slider-inner"
+    field: ".offer__slider-inner",
   });
   calc();
   cards();
